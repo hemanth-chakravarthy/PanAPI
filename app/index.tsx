b12b8668f95f41877d788d0a2bd4bfecad8f0f79
Prasad528260 <prasadsubhedar5460@gmail.com>
2025-06-11 11:24:40 +0530

    added gitignore file

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,13 @@
 import { Text, View } from "react-native";
+import React from "react";
 
 export default function Index() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        flex: 1
+        justifyContent: "center"
+        alignItems: "center"
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
