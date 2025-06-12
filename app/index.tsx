e73c378602acb051f7de7795bc8571c0abba5cbc
Devill0104 <adityamaurya533@gmail.com>
2025-06-12 16:38:39 +0530

    completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,53 +1,3 @@
-// import { Text, View } from "react-native";
-
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       {/* <Text>Edit app/index.tsx to edit this screen. neuxa te</Text> */}
-//       <Text> neuxa team </Text>
-//     </View>
-//   );
-// }
-
-
-
-// import { Text, View, TouchableOpacity } from "react-native";
-// import { Link } from "expo-router";
-
-// export default function Index() {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         justifyContent: "center",
-//         alignItems: "center",
-//         gap: 20,
-//       }}
-//     >
-//       <Text style={{ fontSize: 24, fontWeight: "bold" }}>neuxa team</Text>
-//       {/* <Link href="/seller_login/register" asChild> */}
-//       <Link href="/seller_login/register" asChild>
-//         <TouchableOpacity
-//           style={{
-//             backgroundColor: "#007bff",
-//             padding: 15,
-//             borderRadius: 5,
-//           }}
-//         >
-//           <Text style={{ color: "white", fontWeight: "bold" }}>
-//             Go to Seller Registration
-//           </Text>
-//         </TouchableOpacity>
-//       </Link>
-//     </View>
-//   );
-// }
 
 
 import { Text, View, TouchableOpacity } from "react-native";
@@ -92,6 +42,34 @@ export default function Index() {
           </Text>
         </TouchableOpacity>
       </Link>
+      <Link href="/company/CompanyDetails" asChild>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#28a745",
+            padding: 15,
+            borderRadius: 5,
+          }}
+        >
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            Company Details
+          </Text>
+        </TouchableOpacity>
+      </Link>
+      <Link href="/company/Step2" asChild>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#28a745",
+            padding: 15,
+            borderRadius: 5,
+          }}
+        >
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            Step2
+          </Text>
+        </TouchableOpacity>
+        
+      </Link>
+      
     </View>
   );
 }
