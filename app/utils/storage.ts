b4d24029e54ec358e291307ee0e1bf5899415991
Venkey-doki <venkatesdoki@gmail.com>
2025-07-04 17:11:04 +0530

    added the form handleing , otp length verification, and integrated the backend with the registeration page

---
+++ app/utils/storage.ts
@@ -0,0 +1,14 @@
+// app/utils/storage.ts
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeToken = async (token: string) => {
+  await AsyncStorage.setItem('auth_token', token);
+};
+
+export const getToken = async () => {
+  return await AsyncStorage.getItem('auth_token');
+};
+
+export const removeToken = async () => {
+  await AsyncStorage.removeItem('auth_token');
+};
